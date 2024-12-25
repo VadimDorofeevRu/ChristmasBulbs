0fadb2b0cbb2917fbd15aea291ba4c7b2d53cecc
VadimDorofeevRu <150513277+VadimDorofeevRu@users.noreply.github.com>
2024-12-25 21:22:34 +0700

    Улучшение стиля кода

--- bulbs.js
+++ bulbs.js
@@ -7,49 +7,52 @@ function randomInt(max) {
 class Bulb {
 	constructor(options) {
 		this.options = options;
-		this.colorIndex = 0;
 
 		let container_width = Math.max(options.bulb_size, options.holder_size);
 		let container_height = options.holder / 2 + options.bulb_size;
 
-		this.svg = document.createElementNS(NS, 'svg');
-		this.svg.style.position = 'fixed';
-		this.svg.style.top = '0px';
-		this.svg.style.width = `${container_width}px`;
-		this.svg.style.height = `${container_height}px`;
-		this.svg.style['z-index'] = 9000;
-		document.body.appendChild(this.svg);
+		this.#svg = document.createElementNS(NS, 'svg');
+		this.#svg.style.position = 'fixed';
+		this.#svg.style.top = '0px';
+		this.#svg.style.width = `${container_width}px`;
+		this.#svg.style.height = `${container_height}px`;
+		this.#svg.style['z-index'] = 9000;
+		document.body.appendChild(this.#svg);
 
-		this.bulb = document.createElementNS(NS, 'circle');
-		this.bulb.setAttributeNS(null, 'cx', `${options.bulb_size / 2}px`);
-		this.bulb.setAttributeNS(null, 'cy', `${options.bulb_size / 2 + options.holder_size / 2 - 1}px`);
-		this.bulb.setAttributeNS(null, 'r', `${options.bulb_size / 2}px`);
-		this.bulb.setAttributeNS(null, 'fill', options.colors[randomInt(options.colors.length)]);
-		this.svg.appendChild(this.bulb);
+		this.#bulb = document.createElementNS(NS, 'circle');
+		this.#bulb.setAttributeNS(null, 'cx', `${options.bulb_size / 2}px`);
+		this.#bulb.setAttributeNS(null, 'cy', `${options.bulb_size / 2 + options.holder_size / 2 - 1}px`);
+		this.#bulb.setAttributeNS(null, 'r', `${options.bulb_size / 2}px`);
+		this.#bulb.setAttributeNS(null, 'fill', options.colors[randomInt(options.colors.length)]);
+		this.#svg.appendChild(this.#bulb);
 
-		this.holder = document.createElementNS(NS, 'rect');
-		this.holder.setAttributeNS(null, 'x', `${Math.floor(options.bulb_size / 2 - options.holder_size / 2)}px`);
-		this.holder.setAttributeNS(null, 'y', `0px`);
-		this.holder.setAttributeNS(null, 'width', `${options.holder_size}px`);
-		this.holder.setAttributeNS(null, 'height', `${options.holder_size / 2}px`);
-		this.holder.setAttributeNS(null, 'fill', `gray`);
-		this.svg.appendChild(this.holder);
+		let holder = document.createElementNS(NS, 'rect');
+		holder.setAttributeNS(null, 'x', `${Math.floor(options.bulb_size / 2 - options.holder_size / 2)}px`);
+		holder.setAttributeNS(null, 'y', `0px`);
+		holder.setAttributeNS(null, 'width', `${options.holder_size}px`);
+		holder.setAttributeNS(null, 'height', `${options.holder_size / 2}px`);
+		holder.setAttributeNS(null, 'fill', `gray`);
+		this.#svg.appendChild(holder);
 	}
 
+	#svg = null;
+	#bulb = null;
+	#cord = null;
+
 	get left() {
-		return this.svg.style.left;
+		return this.#svg.style.left;
 	}
 
 	set left(value) {
-		this.svg.style.left = `${value}px`;
+		this.#svg.style.left = `${value}px`;
 	}
 
 	get color() {
-		return this.bulb.getAttributeNS(null, 'fill');
+		return this.#bulb.getAttributeNS(null, 'fill');
 	}
 
 	set color(value) {
-		this.bulb.setAttributeNS(null, 'fill', value);
+		this.#bulb.setAttributeNS(null, 'fill', value);
 	}
 
 	addCord(x1, x2) {
@@ -58,25 +61,25 @@ class Bulb {
 		let y = (x2 - x1) / 7;
 		let d = `M 0,0 C ${m1},${y} ${m2},${y} ${x2 - x1},0`;
 
-		this.cord = document.createElementNS(NS, 'svg');
-		this.cord.style.position = 'fixed';
-		this.cord.style.top = '0px';
-		this.cord.style.left = `${x1}px`;
-		this.cord.style.width = `${x2 - x1}px`;
-		this.cord.style.height = `${y}px`;
-		this.cord.style['z-index'] = 8999;
-		document.body.appendChild(this.cord);
+		this.#cord = document.createElementNS(NS, 'svg');
+		this.#cord.style.position = 'fixed';
+		this.#cord.style.top = '0px';
+		this.#cord.style.left = `${x1}px`;
+		this.#cord.style.width = `${x2 - x1}px`;
+		this.#cord.style.height = `${y}px`;
+		this.#cord.style['z-index'] = 8999;
+		document.body.appendChild(this.#cord);
 
 		let path = document.createElementNS(NS, 'path');
 		path.setAttributeNS(null, 'd', d);
 		path.setAttributeNS(null, 'stroke', 'silver');
 		path.setAttributeNS(null, 'fill', 'none');
-		this.cord.appendChild(path);
+		this.#cord.appendChild(path);
 	}
 
 	dispose() {
-		this.svg?.remove();
-		this.cord?.remove();
+		this.#svg?.remove();
+		this.#cord?.remove();
 	}
 }
 
@@ -125,23 +128,28 @@ export default class Bulbs {
 
 			prev_center = center;
 		}
+		this.#recolorBulbs();
 		this.start();
 	}
 
+	#timer = null;
+
+	#recolorBulbs() {
+		let colors = [...this.options.colors];
+		colors.sort(() => Math.random() - 0.5);
+		for (let i = 0; i < this.bulbs.length; i++)
+			this.bulbs[i].color = colors[i % colors.length];
+	}
+
 	start() {
-		if (this.timer === undefined)
-			this.timer = setInterval(() => {
-				let colors = [...this.options.colors];
-				colors.sort(() => Math.random() - 0.5);
-				for (let i = 0; i < this.bulbs.length; i++)
-					this.bulbs[i].color = colors[i % colors.length];
-			}, this.options.frequency);
+		if (this.#timer === null)
+			this.#timer = setInterval((() => { this.#recolorBulbs() }).bind(this), this.options.frequency);
 	}
 
 	stop() {
-		if (this.timer !== undefined) {
-			clearInterval(this.timer);
-			this.timer = undefined;
+		if (this.#timer !== null) {
+			clearInterval(this.#timer);
+			this.#timer = null;
 			for (let i = 0; i < this.bulbs.length; i++)
 				this.bulbs[i].color = this.options.color_off;
 		}
